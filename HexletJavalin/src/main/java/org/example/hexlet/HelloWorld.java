1e2f018b61cb8a59db4950c7982b18d49bcd38e0
error4071 <sergey_tsoy87@mail.ru>
2023-10-10 22:59:45 +0500

    add src

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,9 +4,11 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
+
         app.get("/", ctx -> ctx.result("Hello World"));
         app.start(7070);
     }
